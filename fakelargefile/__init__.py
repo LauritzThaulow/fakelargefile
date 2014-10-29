ec556fdd8297759020bd81ecd8c8eb82610e3bac
Lauritz V. Thaulow <lauritz.thaulow@gmail.com>
2014-10-29 22:43:35 +0100

    Import segment classes to pacage namespace

--- fakelargefile/__init__.py
+++ fakelargefile/__init__.py
@@ -7,3 +7,5 @@ from __future__ import absolute_import
 
 from fakelargefile.errors import NotFoundError
 from fakelargefile.fakelargefile import FakeLargeFile
+from fakelargefile.segment import (
+    LiteralSegment, RepeatingSegment, HomogenousSegment)
